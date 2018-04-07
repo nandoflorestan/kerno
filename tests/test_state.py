e38385c574ec926a61aa4c9b0bad7851d28886e7
Nando Florestan <nandoflorestan@gmail.com>
2018-04-07 17:01:57 +0200

    kerno.state test coverage is now 100%

---
+++ tests/test_state.py
@@ -0,0 +1,102 @@
+"""Tests for the kerno.web.to_dict module."""
+
+from collections import OrderedDict
+from unittest import TestCase
+from kerno.state import to_dict, UIMessage, Rezulto, MalbonaRezulto
+
+
+class TestUIMessage(TestCase):
+    """Test cases for the UIMessage class and to_dict()."""
+
+    def _make_one(self):
+        return UIMessage(title="Prokofiev is the best!",
+                         plain="We like Prokofiev here.", kind="error")
+
+    def test_ui_message_to_dict(self):
+        examined = self._make_one()
+        adict = to_dict(examined)
+        assert isinstance(adict, OrderedDict)
+        assert adict['title'] == "Prokofiev is the best!"
+        assert adict['plain'] == "We like Prokofiev here."
+        assert adict['html'] == ""
+        with self.assertRaises(KeyError):
+            adict['KINDS']
+
+    def test_ui_message_repr(self):
+        examined = self._make_one()
+        astr = repr(examined)
+        assert isinstance(astr, str)
+        assert astr == '<UIMessage "Prokofiev is the best!">'
+
+
+class TestRezulto(TestCase):
+    """Test cases for the Rezulto class and to_dict()."""
+
+    def _make_one(self):
+        rez = Rezulto()
+        rez.add_toast(title="Prokofiev is the best!",
+                      plain="We like Prokofiev here.")
+        rez.redirect = '/'
+        rez.debug = {'user': 'Mr. Golden Ears'}
+        rez.payload = {"genius": "Prokofiev"}
+        return rez
+
+    def test_rezulto_to_dict(self):
+        examined = self._make_one()
+        adict = to_dict(examined)
+        assert isinstance(adict, OrderedDict)
+        assert adict['status_int'] == 200
+        assert adict['kind'] == 'success'
+        assert adict['messages'] == []
+        assert adict['toasts'] == [OrderedDict([
+            ('html', ''),
+            ('kind', 'success'),
+            ('plain', 'We like Prokofiev here.'),
+            ('title', 'Prokofiev is the best!'),
+        ])]
+        assert adict['debug'] == {'user': 'Mr. Golden Ears'}
+        assert adict['redirect'] == '/'
+        assert adict['payload'] == {"genius": "Prokofiev"}
+        with self.assertRaises(KeyError):
+            adict['KINDS']
+
+    def test_rezulto_repr(self):
+        examined = self._make_one()
+        astr = repr(examined)
+        assert isinstance(astr, str)
+        assert astr == '<Rezulto status: 200>'
+
+
+class TestMalbonaRezulto(TestCase):
+    """Test cases for the MalbonaRezulto class and to_dict()."""
+
+    def _make_one(self):
+        rez = MalbonaRezulto(title="Prokofiev is the best!",
+                             plain="We like Prokofiev here.")
+        rez.redirect = '/'
+        rez.debug = {'user': 'Mr. Golden Ears'}
+        return rez
+
+    def test_malbona_to_dict(self):
+        examined = self._make_one()
+        adict = to_dict(examined)
+        assert isinstance(adict, OrderedDict)
+        assert adict['status_int'] == 400
+        assert adict['kind'] == 'danger'
+        assert adict['toasts'] == []
+        assert adict['messages'] == [OrderedDict([
+            ('html', ''),
+            ('kind', 'danger'),
+            ('plain', 'We like Prokofiev here.'),
+            ('title', 'Prokofiev is the best!'),
+        ])]
+        assert adict['debug'] == {'user': 'Mr. Golden Ears'}
+        assert adict['redirect'] == '/'
+        with self.assertRaises(KeyError):
+            adict['payload']
+
+    def test_malbona_repr(self):
+        examined = self._make_one()
+        astr = repr(examined)
+        assert isinstance(astr, str)
+        assert astr == '<MalbonaRezulto status: 400>'
