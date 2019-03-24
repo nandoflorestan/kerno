2016605b8bc98e0e5d45f3d9f0a12d7c728e7447
Nando Florestan <nandoflorestan@gmail.com>
2019-03-24 19:10:40 +0100

    Extract method that returns the arguments to Message

--- kerno/email/pyramid_mailer.py
+++ kerno/email/pyramid_mailer.py
@@ -1,6 +1,6 @@
 """A complement to our email module, for those who use pyramid_mailer."""
 
-from typing import Dict
+from typing import Any, Dict
 
 from pyramid_mailer.message import Message
 
@@ -15,17 +15,24 @@ class EmailMessage(EmailMessageBase):
         super().__init__(**kw)
         self.mailer_settings = mailer_settings
 
-    def to_message(self) -> Message:
-        """Convert this instance into a pyramid_mailer Message."""
+    def to_message_args(self) -> Dict[str, Any]:
+        """Convert this into a dict to instantiate a pyramid_mailer Message.
+
+        This is useful because Celery likes to use JSON serialization.
+        """
         adict = self.to_dict()
         env = adict['envelope']
-        return Message(
-            subject=adict['subject'],
-            html=adict['html'],
-            body=adict['plain'],
-            recipients=env['recipients'],
-            sender=env['sender'] or self.mailer_settings["default_sender"],
-            cc=env['cc'],
-            bcc=env['bcc'],
-            extra_headers={'reply_to': env['reply_to']},
-        )
+        return {
+            'subject': adict['subject'],
+            'html': adict['html'],
+            'body': adict['plain'],
+            'recipients': env['recipients'],
+            'sender': env['sender'] or self.mailer_settings["default_sender"],
+            'cc': env['cc'],
+            'bcc': env['bcc'],
+            'extra_headers': {'reply_to': env['reply_to']},
+        }
+
+    def to_message(self) -> Message:
+        """Convert this instance into a pyramid_mailer Message."""
+        return Message(**self.to_message_args())
