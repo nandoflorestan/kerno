984ef4a9e125dbe1203c16d8f2103361361844ac
Nando Florestan <nandoflorestan@gmail.com>
2019-12-13 11:58:03 +0100

    Add the NumLines colander validator

--- kerno/colander.py
+++ kerno/colander.py
@@ -25,3 +25,28 @@ def validate_schema(
         malbona.add_toast(title=mal_title, plain=mal_plain)
         malbona.invalid = e.asdict()
         raise malbona
+
+
+class NumLines:
+    """Colander validator that checks the number of lines in text."""
+
+    def __init__(
+        self,
+        min: int = 0,
+        max: int = -1,
+        min_err: str = "Not enough lines (minimum {min})",
+        max_err: str = "Too many lines (maximum {max})",
+    ) -> None:  # noqa
+        self.min = min
+        self.max = max
+        self.min_err = min_err
+        self.max_err = max_err
+
+    def __call__(self, node, value) -> None:  # noqa
+        num_lines = value.count("\n") + 1
+        if self.min > 0 and num_lines < self.min:
+            # err = _(self.min_err, mapping={"min": self.min})
+            raise c.Invalid(node, self.min_err.format(min=self.min))
+        if self.max > -1 and num_lines > self.max:
+            # err = _(self.max_err, mapping={"max": self.max})
+            raise c.Invalid(node, self.max_err.format(max=self.max))
