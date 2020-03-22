3ead5dd7a78200f60610a2a69f727e2a976c117e
Nando Florestan <nandoflorestan@gmail.com>
2020-03-22 14:27:30 +0100

    Bump version to 0.4.1.dev1 after release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url="https://github.com/nandoflorestan/kerno",
     name="kerno",
     author="Nando Florestan",
-    version="0.4.0",
+    version="0.4.1.dev1",
     license="MIT",
     # https://setuptools.readthedocs.io/en/latest/setuptools.html#using-find-packages
     packages=find_packages(exclude=["tests.*", "tests"]),
