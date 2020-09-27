6cf4d8e3a21ba8146bc84c0516046154fb665a7b
Nando Florestan <nandoflorestan@gmail.com>
2020-09-27 05:55:39 +0200

    Add the Python side of jsonright

---
+++ kerno/web/jsonright.py
@@ -0,0 +1,173 @@
+"""A better way to send entities through the wire.
+
+What problems does this solve?
+
+1. Python model classes do not need to implement ``as_dict()``. Instead, you
+implement a variation of a ``jsonright()`` function in a separate step,
+which could even be in a separate package, totally decoupled. This does
+[single dispatch](https://docs.python.org/3/library/functools.html#functools.singledispatch) -- please learn about this.
+
+2. When we send objects through the wire, different detail levels are
+commonly needed -- for instance, first we only want the names of users to be
+shown in a list, and then in a separate step, we might want all the details
+of a single user. Your ``jsonright()`` implementation receives a ``features``
+argument which can be a set of strings (or a sequence of any type)
+specifying the level of detail. This keeps the API uniform.
+
+3. This module also offers tools to help you write those
+``jsonright()`` implementations -- especially ``entity2dict()``.
+
+4. Sending common JSON through the wire has a problem: the repetition of keys.
+For example, if you output a list of users, the keys of each object, such as
+"id", "name", "email" etc. are repeated for each instance in the JSON.
+Here we save bandwidth by encoding the data differently and then
+reassembling it in the client.
+
+5. The ``jsonright()`` interface has a ``peto`` argument, therefore when
+implementing it you have access to the context, including application
+configuration, repository (data access layer), current user etc.
+
+How are data organized to save bandwidth?
+=========================================
+
+jsonright avoids repeating key names when outputting sequences of entities
+in JSON. It pivots the table so it grows to the right:
+
+    [
+        ["id", 1, 2],
+        ["email", "ex@am.pl", "sagan@nasa.gov"],
+    ]
+
+Of course objects are easily reassembled in Javascript.
+
+Usage
+=====
+
+Please see a usage example in
+`our tests <https://github.com/nandoflorestan/kerno/blob/master/tests/test_web_jsonright.py>`_.
+"""
+
+from datetime import date, datetime
+from decimal import Decimal
+from functools import singledispatch
+from typing import Any, Iterable, Sequence
+
+from bag import first
+from kerno.peto import Peto
+from kerno.typing import DictStr
+
+
+def keys_from(obj: Any) -> Iterable[str]:
+    """Return the names of the instance variables of ``obj``."""
+    return vars(obj).keys()
+
+
+def only_relevant(keys: Iterable[str]) -> Iterable[str]:
+    """Ignore strings that start in dunder ("__") or in "_sa_".
+
+    These usually keep SQLAlchemy state.
+    """
+    return filter(
+        lambda key: not key.startswith("__") and not key.startswith("_sa_"),
+        keys,
+    )
+
+
+def excluding(
+    blacklist: Sequence[str], keys: Iterable[str]
+) -> Iterable[str]:  # noqa
+    return filter(lambda k: k not in blacklist, keys)
+
+
+def entity2dict(
+    obj: Any,
+    keys: Iterable[str] = (),
+) -> DictStr:
+    """Dump certain instance variables of ``obj`` into a dictionary.
+
+    This function is reusable.
+
+    If you do not provide any ``keys``, a sensible default is used.
+    """
+    kk = keys or excluding(("password",), only_relevant(keys_from(obj)))
+    return {key: getattr(obj, key) for key in kk}
+
+
+@singledispatch
+def jsonright(obj: Any, peto: Peto, features=(), **kw) -> Any:
+    """Overloadable function to encode entities for sending through the wire.
+
+    You can register your own implementations which get called depending
+    on the type of *obj*.
+    """
+    raise NotImplementedError(
+        f"No implementation of jsonright() registered for type {type(obj)}"
+    )
+
+
+@jsonright.register(str)
+@jsonright.register(int)
+@jsonright.register(float)
+@jsonright.register(bool)
+@jsonright.register(type(None))
+def _(obj, peto: Peto, features=(), **kw) -> Any:
+    return obj
+
+
+@jsonright.register(bytes)
+def _(obj, peto: Peto, features=(), **kw) -> Any:
+    return obj.decode(kw.get("encoding", "utf-8"))
+
+
+@jsonright.register(Decimal)
+def _(obj, peto: Peto, features=(), **kw) -> float:
+    return float(str(obj))
+
+
+@jsonright.register(datetime)
+@jsonright.register(date)
+def _(obj, peto: Peto, features=(), **kw) -> str:
+    return obj.isoformat()
+
+
+@jsonright.register(dict)
+def _(obj, peto: Peto, features=(), **kw) -> DictStr:
+    return {
+        str(key): jsonright(val, peto, features, **kw)
+        for (key, val) in obj.items()
+    }
+
+
+primitive_types = (
+    str,
+    bytes,
+    int,
+    float,
+    bool,
+    type(None),
+    Decimal,
+    date,
+    datetime,
+    dict,
+)
+
+
+@jsonright.register(list)
+@jsonright.register(tuple)
+@jsonright.register(set)
+@jsonright.register(frozenset)
+def _(obj, peto: Peto, features=(), **kw) -> Sequence:
+    if len(obj) == 0:
+        return []
+    first_item = first(obj)
+    if isinstance(first_item, primitive_types):
+        return [jsonright(item, peto, features, **kw) for item in obj]
+    # Below this line we assume we are dealing with a sequence of entities.
+    # In this case we pivot data in order to save bandwidth.
+    first_dict = jsonright(first_item, peto, features, **kw)
+    ret = [[key] for key in first_dict]
+    for entity in obj:
+        adict = jsonright(entity, peto, features, **kw)
+        for alist in ret:
+            alist.append(jsonright(adict[alist[0]], peto, features, **kw))
+    return ret
---
+++ tests/test_web_jsonright.py
@@ -0,0 +1,127 @@
+"""Tests for the kerno.web.jsonright module."""
+
+from datetime import datetime
+from unittest import TestCase
+
+from kerno.typing import DictStr
+from kerno.web.jsonright import entity2dict, jsonright
+
+
+class TestDefaultJsonrightImplementation(TestCase):
+    """Test cases for our default jsonright() implementation."""
+
+    def test_str(self):  # noqa
+        assert jsonright("Adam Harasiewicz", None) == "Adam Harasiewicz"
+
+    def test_int(self):  # noqa
+        assert jsonright(42, None) == 42
+
+    def test_float(self):  # noqa
+        assert jsonright(3.1415, None) == 3.1415
+
+    def test_bool(self):  # noqa
+        assert jsonright(False, None) is False
+
+    def test_datetime(self):  # noqa
+        assert (
+            jsonright(datetime(2020, 9, 27, 4, 55, 42), None)
+            == "2020-09-27T04:55:42"
+        )
+
+    def test_dict(self):  # noqa
+        assert jsonright({"id": 1, "date": datetime(2020, 9, 27)}, None) == {
+            "id": 1,
+            "date": "2020-09-27T00:00:00",
+        }
+
+    def test_tuple_of_dicts(self):  # noqa
+        assert jsonright(
+            ({"id": 1, "date": datetime(2020, 9, 27)},), None
+        ) == [
+            {
+                "id": 1,
+                "date": "2020-09-27T00:00:00",
+            }
+        ]
+
+    def test_frozenset(self):  # noqa
+        assert jsonright(frozenset((1, 2)), None) == [1, 2]
+
+
+class MyModel:  # noqa
+    def __init__(self):  # noqa
+        self.name = "Nando Florestan"
+        self.profession1 = "Python developer"
+        self.birth = datetime(1976, 7, 18)
+        self.password = "Krystian Zimerman"  # we never want passwords in JSON
+
+
+@jsonright.register(MyModel)
+def _(obj, peto, features=(), **kw) -> DictStr:
+    if "MyModel unsafe" in features:
+        return entity2dict(obj, keys=["name", "password"])
+    else:
+        return entity2dict(obj)
+
+
+class TestMyModel(TestCase):  # noqa
+    def test_mymodel(self):  # noqa
+        entity = MyModel()
+        right = jsonright(entity, None)
+        assert isinstance(right, dict)
+        assert right["name"] == "Nando Florestan"
+        assert right["profession1"] == "Python developer"
+        assert right["birth"] == datetime(1976, 7, 18, 0, 0)
+        with self.assertRaises(KeyError):
+            right["password"]
+
+    def test_mymodel_unsafe(self):  # noqa
+        entity = MyModel()
+        right = jsonright(entity, None, features=("MyModel unsafe",))
+        assert isinstance(right, dict)
+        assert right["name"] == "Nando Florestan"
+        assert right["password"] == "Krystian Zimerman"
+        with self.assertRaises(KeyError):
+            right["birth"]
+        with self.assertRaises(KeyError):
+            right["profession1"]
+
+
+class MyModelSubclass(MyModel):  # noqa
+    def __init__(self):  # noqa
+        super().__init__()
+        self.profession2 = "Classical music composer"
+
+
+@jsonright.register(MyModelSubclass)
+def _(obj, peto, features=(), **kw) -> DictStr:
+    amap = entity2dict(obj)
+    # This version includes even the class name:
+    amap["__class__"] = MyModelSubclass.__name__
+    return amap
+
+
+class TestMyModelSubclass(TestCase):  # noqa
+    def test_subclass(self):  # noqa
+        entity = MyModelSubclass()
+        right = jsonright(entity, None)
+        assert isinstance(right, dict)
+        assert right["name"] == "Nando Florestan"
+        assert right["birth"] == datetime(1976, 7, 18, 0, 0)
+        assert right["profession1"] == "Python developer"
+        assert right["profession2"] == "Classical music composer"
+        assert right["__class__"] == "MyModelSubclass"
+        with self.assertRaises(KeyError):
+            right["password"]
+
+
+class TestListOfEntities(TestCase):  # noqa
+    def test_sequence(self):  # noqa
+        payload = [MyModel()]
+        right = jsonright(payload, None)
+        assert isinstance(right, list)
+        assert right == [
+            ["name", "Nando Florestan"],
+            ["profession1", "Python developer"],
+            ["birth", "1976-07-18T00:00:00"],
+        ]
