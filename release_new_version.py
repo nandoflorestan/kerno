7f7ebb1257177163ae2b7fce22d476bd7ca0214a
Nando Florestan <nandoflorestan@gmail.com>
2022-02-16 14:02:46 +0100

    Update releaser script;  goodbye, setup.py

--- release_new_version.py
+++ release_new_version.py
@@ -6,13 +6,11 @@ from releaser import Releaser  # pip install releaser
 from releaser.steps import (
     Shell,
     CheckRstFiles,
-    InteractivelyApproveDistribution,
-    InteractivelyApproveWheel,
-    CheckTravis,
-    SetVersionNumberInteractively,
-    PypiUpload,
-    PypiUploadWheel,
+    InteractivelyApprovePackage,
     SetFutureVersion,
+    SetVersionNumberInteractively,
+    TwineUploadSource,
+    TwineUploadWheel,
     Warn,
 )
 from releaser.git_steps import (
@@ -31,7 +29,7 @@ config = dict(
     github_repository="kerno",
     branch="master",  # Only release new versions in this git branch
     changes_file=None,
-    version_file="setup.py",  # Read and write version number on this file
+    version_file="pyproject.toml",  # Read and write version number on this file
     version_keyword="version",  # Part of the variable name in that file
     log_file="release.log.utf-8.tmp",
     verbosity="info",  # debug | info | warn | error
@@ -42,30 +40,31 @@ config = dict(
 Releaser(
     config,
     # ==================  Before releasing, do some checks  ===================
-    Shell("python setup.py test"),  # First of all ensure tests pass
+    Shell("py.test -s --tb=native tests"),  # First of all ensure tests pass
     # CheckRstFiles,  # Documentation: recursively verify ALL .rst files, or:
     CheckRstFiles("README.rst", "docs/LICENSE.rst"),  # just a few.
-    # TODO IMPLEMENT CompileAndVerifyTranslations,
     EnsureGitClean,  # There are no uncommitted changes in tracked files.
     EnsureGitBranch,  # I must be in the branch specified in config
     # InteractivelyEnsureChangesDocumented,     # Did you update CHANGES.rst?
+    # Shell("poetry install")  # Ensure the package can be installed
     # CheckTravis,  # We run this late, so travis-ci has more time to build
     # ======================  All checks pass. RELEASE!  ======================
     SetVersionNumberInteractively,  # Ask for version and write to source code
     Shell("./build_sphinx_documentation.sh"),  # You can write it easily
+    Shell("poetry build"),  # Build sdist + wheel with poetry
+    # Shell("python setup.py sdist"),  # Build source distribution with setuptools
+    # Shell("python setup.py bdist_wheel"),  # Build binary wheel with setuptools
+    InteractivelyApprovePackage,  # Ask user to manually verify wheel content
     GitCommitVersionNumber,
     GitTag,  # Locally tag the current commit with the new version number
-    InteractivelyApproveDistribution,  # Generate sdist, let user verify it
-    InteractivelyApproveWheel,         # Generate wheel, let user verify it
-    PypiUpload,  # Make and upload a source .tar.gz to https://pypi.org
-    PypiUploadWheel,  # Make and upload source wheel to https://pypi.org
+    Shell("poetry publish"),  # Upload source and wheel to https://pypi.org
+    # TwineUploadSource,  # Upload a source .tar.gz to https://pypi.org with Twine
+    # TwineUploadWheel,  # Upload wheel to https://pypi.org with Twine
     # ===========  Post-release: set development version and push  ============
-    SetFutureVersion,  # Writes incremented version, now with 'dev' suffix
-    GitCommitVersionNumber(
-        "future_version", msg="Bump version to {0} after release"
-    ),
+    SetFutureVersion,  # Writes incremented version, now with 'dev1' suffix
+    GitCommitVersionNumber("future_version", msg="Bump version to {0} after release"),
     # ErrorStep,  # You can use this step while testing - it causes a rollback.
     GitPush,  # Cannot be undone. If successful, previous steps won't roll back
     GitPushTags,
-    Warn("Do not forget to upload the documentation!"),
+    Warn("Do not forget to upload the documentation now!"),
 ).release()
