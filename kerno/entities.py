9a70c7efd68717b22a3b2ba89685e1bfb72f0c65
Nando Florestan <nandoflorestan@gmail.com>
2024-01-09 15:04:03 +0100

    Add entities.py: file name validation

---
+++ kerno/entities.py
@@ -0,0 +1,29 @@
+"""Reusable base entities."""
+
+from base64 import b64decode
+
+
+class FileName(str):  # noqa
+    def __init__(self, filename: str, max=50) -> None:  # noqa
+        super().__init__(filename.strip())
+        length = len(self)
+        assert length > 2, "The file name is too short; min: 3 characters."
+        if max != 0:
+            assert length <= max, f"The file name is too long; max: {max} characters."
+        assert not self.endswith("."), "A file name must not end with a dot."
+        assert "." in self, "A file name must have an extension."
+        assert "/" not in self, "A file name must not contain a slash."
+
+    @property
+    def extension(self):  # noqa
+        return self.split(".")[-1]
+
+    @property
+    def title(self):  # noqa
+        return self[: -(len(self.extension) + 1)]
+
+
+class UploadedFile:  # noqa
+    def __init__(self, filename: str, payload: str) -> None:  # noqa
+        self.byts: bytes = b64decode(payload)
+        self.filename = FileName(filename)
