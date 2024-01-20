a220d4f324c31674ea2df293d9cab142509e316b
Nando Florestan <nandoflorestan@gmail.com>
2024-01-20 13:50:01 +0100

    Wrap instead of subclassing str

--- kerno/entities.py
+++ kerno/entities.py
@@ -3,27 +3,28 @@
 from base64 import b64decode
 
 
-class FileName(str):  # noqa
-
-    def __new__(self, value, **kw):  # noqa
-        super().__new__(self, value.strip())
+class FileName:  # noqa
 
     def __init__(self, value: str, *, max=50) -> None:  # noqa
-        length = len(self)
+        self.value = value.strip()
+        length = len(self.value)
         assert length > 2, "The file name is too short; min: 3 characters."
         if max != 0:
             assert length <= max, f"The file name is too long; max: {max} characters."
-        assert not self.endswith("."), "A file name must not end with a dot."
-        assert "." in self, "A file name must have an extension."
-        assert "/" not in self, "A file name must not contain a slash."
+        assert not self.value.endswith("."), "A file name must not end with a dot."
+        assert "." in self.value, "A file name must have an extension."
+        assert "/" not in self.value, "A file name must not contain a slash."
 
     @property
     def extension(self):  # noqa
-        return self.split(".")[-1]
+        return self.value.split(".")[-1]
 
     @property
     def title(self):  # noqa
-        return self[: -(len(self.extension) + 1)]
+        return self.value[: -(len(self.extension) + 1)]
+
+    def __str__(self):
+        return self.value
 
 
 class UploadedFile:  # noqa
