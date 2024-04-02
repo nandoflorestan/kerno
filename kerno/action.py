5bc6b5acae7b7deb690d87beeba3b6eafbe71f53
Nando Florestan <nandoflorestan@gmail.com>
2024-04-02 11:34:33 +0200

    Add EntityComparison; use it in OrganizeValueObjects

--- kerno/action.py
+++ kerno/action.py
@@ -3,7 +3,8 @@
 Also a DEPRECATED abstract base for class-based actions.
 """
 
-from typing import Generic, Iterable
+from typing import Any, Generic, Iterable, Sequence
+from warnings import warn
 
 from abc import ABCMeta
 
@@ -26,6 +27,7 @@ class Action(metaclass=ABCMeta):
     """
 
     def __init__(self, peto: AbstractPeto):  # noqa
+        warn("kerno.Action is deprecated and will be removed.", DeprecationWarning)
         self.peto = peto
 
     @property
@@ -42,6 +44,42 @@ class Action(metaclass=ABCMeta):
     def user(self):
         """Return the current user or None."""
         return self.peto.user
+
+
+class EntityComparison(Generic[Entity]):
+    """Find the differences between two entities."""
+
+    def __init__(self, old: Entity, new: Entity):  ## noqa
+        self.old = old
+        self.new = new
+
+    def get_differences(
+        self, var_names: Sequence[str] = ()
+    ) -> list[tuple[str, Any, Any]]:
+        """Find the differences between the two entities.
+
+        Pay attention only to the relevant *var_names*.
+        """
+        if not var_names:
+            var_names = list(self.old.__dict__.keys())
+            if "password" in var_names:
+                var_names.remove("password")
+        ret = []
+        for name in var_names:
+            old_value = getattr(self.old, name)
+            new_value = getattr(self.new, name)
+            if old_value != new_value:
+                ret.append((name, old_value, new_value))
+        return ret
+
+    @staticmethod
+    def to_str(differences: Sequence[tuple[str, Any, Any]], sep=" ") -> str:
+        """Return text stating the differences. The separator is configurable."""
+        diffs = [f"{d[0]} changed from «{d[1]}» to «{d[2]}»." for d in differences]
+        return sep.join(diffs)
+
+    def __str__(self) -> str:  # noqa
+        return self.to_str(self.get_differences())
 
 
 class OrganizeHashableObjects(Generic[Entity]):
@@ -71,8 +109,17 @@ class OrganizeHashableObjects(Generic[Entity]):
 class OrganizeValueObjects(Generic[Entity]):
     """Given current and desired collections, sort objects into to_add and to_remove.
 
-    This version loops testing equality, so the objects
-    must implement __eq__, or be dictionaries or tuples.
+    This version loops testing equality, so the objects must
+    implement __eq__, or be dictionaries or tuples. Providing your own
+    equality function is also possible as *eq_fn*.
+
+    In the output, *to_keep* contains instances of EntityComparison,
+    so it is then possible to find further differences between the
+    existing and desired objects, by considering more variables.
+
+    This helps figure out what objects to delete from a DB, what
+    objects to add, and what objects to alter, with minimal interference
+    in the database (as opposed to crudely deleting and adding everything).
     """
 
     def __init__(
@@ -83,13 +130,13 @@ class OrganizeValueObjects(Generic[Entity]):
     ):
         """Organize through iteration and comparison."""
         self.to_add: list[Entity] = []
-        self.to_keep: list[Entity] = []
         self.to_remove: list[Entity] = []
+        self.to_keep: list[EntityComparison] = []
         for d in desired_objects:
             found = False
             for e in existing_objects:
                 if eq_fn(d, e) if eq_fn else (d == e):
-                    self.to_keep.append(e)
+                    self.to_keep.append(EntityComparison(old=e, new=d))
                     found = True
                     break
             if found:
--- tests/test_action.py
+++ tests/test_action.py
@@ -1,17 +1,49 @@
 """Tests for kerno.action helper classes."""
 
-from kerno.action import OrganizeHashableObjects, OrganizeValueObjects
+from base64 import b64encode
+
+from kerno.action import EntityComparison, OrganizeHashableObjects, OrganizeValueObjects
+from kerno.entities import UploadedFile
 
 
-def test_organizing():  # noqa
-    existing = [("existing and desired",), ("existing but not desired",)]
-    desired = [("existing and desired",), ("new and desired",)]
-    org1 = OrganizeHashableObjects(existing, desired)
-    org2 = OrganizeValueObjects(existing, desired)
-    for org in (org1, org2):
-        assert len(org.to_add) == 1
-        assert len(org.to_keep) == 1
-        assert len(org.to_remove) == 1
-        assert desired[1] in org.to_add
-        assert existing[0] in org.to_keep
-        assert existing[1] in org.to_remove
+def test_EntityComparison():  # noqa
+    old = UploadedFile(filename="autoexec.bat", payload=b64encode(b"@ECHO OFF"))
+    new = UploadedFile(filename="hello_world.py", payload=b64encode(b"print('hi')"))
+    comparison = EntityComparison(old, new)
+    assert comparison.get_differences() == [
+        ("byts", b"@ECHO OFF", b"print('hi')"),
+        ("filename", old.filename, new.filename),
+    ]
+    assert comparison.get_differences(var_names=("byts",)) == [
+        ("byts", b"@ECHO OFF", b"print('hi')"),
+    ]
+    assert str(comparison) == (
+        "byts changed from «b'@ECHO OFF'» to «b\"print('hi')\"». "
+        "filename changed from «autoexec.bat» to «hello_world.py»."
+    )
+
+
+existing = [("existing and desired",), ("existing but not desired",)]
+desired = [("existing and desired",), ("new and desired",)]
+
+
+def test_OrganizeHashableObjects():  # noqa
+    org = OrganizeHashableObjects(existing, desired)
+    assert len(org.to_add) == 1
+    assert len(org.to_keep) == 1
+    assert len(org.to_remove) == 1
+    assert desired[1] in org.to_add
+    assert existing[0] in org.to_keep
+    assert existing[1] in org.to_remove
+
+
+def test_OrganizeValueObjects():  # noqa
+    org = OrganizeValueObjects(existing, desired)
+    assert len(org.to_add) == 1
+    assert len(org.to_keep) == 1
+    assert len(org.to_remove) == 1
+    assert desired[1] in org.to_add
+    assert existing[1] in org.to_remove
+    comparison = org.to_keep[0]
+    assert existing[0] is comparison.old
+    assert desired[0] is comparison.new
